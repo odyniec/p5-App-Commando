ff0f6a10caab533015e148bfefef48a8cbc2f44b
Michal Wojciechowski <odyniec@cpan.org>
2014-07-22 23:54:19 +0200

    Pass constructor data as an array instead of an arrayref

--- lib/App/Commando/Option.pm
+++ lib/App/Commando/Option.pm
@@ -12,11 +12,11 @@ has 'long' => ( is => 'ro' );
 has 'short' => ( is => 'ro' );
 
 sub BUILDARGS {
-    my ($class, $config_key, $info) = @_;
+    my ($class, $config_key, @info) = @_;
 
     my $buildargs = {};
 
-    for my $arg (@$info) {
+    for my $arg (@info) {
         if ($arg =~ /^-/) {
             if ($arg =~ /^--/) {
                 $buildargs->{long} = $arg;
