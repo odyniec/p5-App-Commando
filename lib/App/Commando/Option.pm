df9ea54d93afad1e4564c8d71ccf8146460b2cdd
Michal Wojciechowski <odyniec@cpan.org>
2014-07-23 09:36:21 +0200

    Add support for option specifications in Getopt::Long format

--- lib/App/Commando/Option.pm
+++ lib/App/Commando/Option.pm
@@ -11,10 +11,24 @@ has 'long' => ( is => 'ro' );
 
 has 'short' => ( is => 'ro' );
 
+has 'spec' => ( is => 'ro' );
+
 sub BUILDARGS {
     my ($class, $config_key, @info) = @_;
 
     my $buildargs = {};
+
+    # Getopt::Long regular expression to match argument specification
+    my $spec_re = qr(
+        # Either modifiers ...
+        [!+]
+        |
+        # ... or a value/dest/repeat specification
+        [=:] [ionfs] [@%]? (?: \{\d*,?\d*\} )?
+        |
+        # ... or an optional-with-default spec
+        : (?: -?\d+ | \+ ) [@%]?
+    )x;
 
     for my $arg (@info) {
         if ($arg =~ /^-/) {
@@ -24,6 +38,9 @@ sub BUILDARGS {
             else {
                 $buildargs->{short} = $arg;
             }
+        }
+        elsif ($arg =~ $spec_re) {
+            $buildargs->{spec} = $arg;
         }
         else {
             $buildargs->{description} = $arg;
@@ -36,7 +53,12 @@ sub BUILDARGS {
 sub for_get_options {
     my ($self) = @_;
 
-    return join '|', grep { /\S/ } map { $self->$_ } qw( short long );
+    return
+        join('|',
+            grep { /\S/ } # Exclude empty strings
+                map { s/^-+//; $_ } # Strip off leading dashes
+                    map { $self->$_ } qw( short long )) .
+        $self->spec;
 }
 
 1;
