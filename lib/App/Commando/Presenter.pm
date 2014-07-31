c605bce0807e82b45f3ad379848959c9ad2a9a6d
Michal Wojciechowski <odyniec@cpan.org>
2014-07-31 09:22:18 +0200

    Add command_header and command_presentation methods to Presenter

--- lib/App/Commando/Presenter.pm
+++ lib/App/Commando/Presenter.pm
@@ -15,4 +15,24 @@ sub BUILDARGS {
     };
 }
 
+sub command_header {
+    my ($self) = @_;
+
+    my $header = $self->command->identity . "\n";
+    $header .= ' -- ' . $self->command->description
+        if $self->command->description;
+
+    return $header;
+}
+
+sub command_presentation {
+    my ($self) = @_;
+
+    my @msg = ();
+
+    push @msg, $self->command_header;
+
+    return join "\n\n", @msg;
+}
+
 1;
