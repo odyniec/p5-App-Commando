e0fa281d99a2a2128bee911b9c7311e2c8dfcbf5
Michal Wojciechowski <odyniec@cpan.org>
2014-08-02 11:09:07 +0200

    Add SYNOPSIS

--- lib/App/Commando.pm
+++ lib/App/Commando.pm
@@ -18,3 +18,34 @@ sub program {
 }
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use App::Commando;
+
+    my $program = App::Commando::program('example');
+    $program->version('0.42');
+
+    my $cmd_hello = $program->command('hello');
+    $cmd_hello->syntax('hello TARGET');
+    $cmd_hello->option('world', '-w', '--world', 'Say hello to the World');
+    $cmd_hello->option('universe', '-u', '--universe', 'Say hello to the Universe');
+    $cmd_hello->action(sub {
+        my ($argv, $config) = @_;
+
+        # Get the first argument or set the default value
+        my $target = $argv->[0] || 'Everyone';
+        $target = "World" if $config->{world};
+        $target = "Universe" if $config->{universe};
+
+        print "Hello, $target!\n";
+    });
+
+    my $cmd_bye = $program->command('bye');
+    $cmd_bye->action(sub {
+        print "Goodbye!\n";
+    });
+
+    $program->go;
